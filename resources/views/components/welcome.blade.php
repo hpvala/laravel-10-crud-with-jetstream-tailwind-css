192f1f8906f969e8f9e75d214be6f2fabdefb1b0
Hitesh <hitesh.v@samcomtechnobrains.com>
2023-05-12 18:50:23 +0530

    Set profile

--- resources/views/components/welcome.blade.php
+++ resources/views/components/welcome.blade.php
@@ -1,19 +1,18 @@
-<div class="p-6 lg:p-8 bg-white border-b border-gray-200">
-    <x-application-logo class="block h-12 w-auto" />
+<div class="p-6 bg-white border-b border-gray-200 lg:p-8">
 
-    <h1 class="mt-8 text-2xl font-medium text-gray-900">
-        Welcome to your Jetstream application!
+    <h1 class="flex mt-2 text-2xl font-medium text-gray-900">
+        <x-application-logo class="block w-auto h-12" />
+        <span class="mt-3 ml-3">
+            Welcome to Hitesh Vala Laravel Livewire Demo
+        </span>
     </h1>
 
-    <p class="mt-6 text-gray-500 leading-relaxed">
-        Laravel Jetstream provides a beautiful, robust starting point for your next Laravel application. Laravel is designed
-        to help you build your application using a development environment that is simple, powerful, and enjoyable. We believe
-        you should love expressing your creativity through programming, so we have spent time carefully crafting the Laravel
-        ecosystem to be a breath of fresh air. We hope you love it.
+    <p class="mt-6 leading-relaxed text-gray-500">
+        We will see laravel 10 livewire crud with jetstream and tailwind CSS. Here, we will learn about how to create a crud application using jetstream and tailwind CSS. Laravel Jetstream is a beautifully designed application starter kit for Laravel and provides the perfect starting point for your next Laravel application.
     </p>
 </div>
 
-<div class="bg-gray-200 bg-opacity-25 grid grid-cols-1 md:grid-cols-2 gap-6 lg:gap-8 p-6 lg:p-8">
+<div class="grid grid-cols-1 gap-6 p-6 bg-gray-200 bg-opacity-25 md:grid-cols-2 lg:gap-8 lg:p-8">
     <div>
         <div class="flex items-center">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" class="w-6 h-6 stroke-gray-400">
@@ -24,7 +23,7 @@
             </h2>
         </div>
 
-        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+        <p class="mt-4 text-sm leading-relaxed text-gray-500">
             Laravel has wonderful documentation covering every aspect of the framework. Whether you're new to the framework or have previous experience, we recommend reading all of the documentation from beginning to end.
         </p>
 
@@ -32,7 +31,7 @@
             <a href="https://laravel.com/docs" class="inline-flex items-center font-semibold text-indigo-700">
                 Explore the documentation
 
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ml-1 w-5 h-5 fill-indigo-500">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="w-5 h-5 ml-1 fill-indigo-500">
                     <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
                 </svg>
             </a>
@@ -49,7 +48,7 @@
             </h2>
         </div>
 
-        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+        <p class="mt-4 text-sm leading-relaxed text-gray-500">
             Laracasts offers thousands of video tutorials on Laravel, PHP, and JavaScript development. Check them out, see for yourself, and massively level up your development skills in the process.
         </p>
 
@@ -57,7 +56,7 @@
             <a href="https://laracasts.com" class="inline-flex items-center font-semibold text-indigo-700">
                 Start watching Laracasts
 
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="ml-1 w-5 h-5 fill-indigo-500">
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" class="w-5 h-5 ml-1 fill-indigo-500">
                     <path fill-rule="evenodd" d="M5 10a.75.75 0 01.75-.75h6.638L10.23 7.29a.75.75 0 111.04-1.08l3.5 3.25a.75.75 0 010 1.08l-3.5 3.25a.75.75 0 11-1.04-1.08l2.158-1.96H5.75A.75.75 0 015 10z" clip-rule="evenodd" />
                 </svg>
             </a>
@@ -74,7 +73,7 @@
             </h2>
         </div>
 
-        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+        <p class="mt-4 text-sm leading-relaxed text-gray-500">
             Laravel Jetstream is built with Tailwind, an amazing utility first CSS framework that doesn't get in your way. You'll be amazed how easily you can build and maintain fresh, modern designs with this wonderful framework at your fingertips.
         </p>
     </div>
@@ -89,7 +88,7 @@
             </h2>
         </div>
 
-        <p class="mt-4 text-gray-500 text-sm leading-relaxed">
+        <p class="mt-4 text-sm leading-relaxed text-gray-500">
             Authentication and registration views are included with Laravel Jetstream, as well as support for user email verification and resetting forgotten passwords. So, you're free to get started with what matters most: building your application.
         </p>
     </div>
